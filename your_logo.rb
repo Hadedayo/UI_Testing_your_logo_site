612dd6dc4137c1258cdbaa53d1519d6ead355f68
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-12-05 21:38:31 +0000

    Creating methods to access data on the your logo website

---
+++ your_logo.rb
@@ -0,0 +1,41 @@
+require 'selenium-webdriver'
+
+class YourLogoFormSelenium
+
+  def initialize
+    @chrome_driver = Selenium::WebDriver.for :chrome
+    @your_logo_url = 'http://automationpractice.com/index.php'
+    @sign_in_link = 'Sign in'
+    @create_user_email = 'email_create'
+    @create_account = 'SubmitCreate'
+  end
+
+  def visit_your_logo_form
+    @chrome_driver.get(@your_logo_url)
+  end
+
+  def sign_into_your_logo_website
+    @chrome_driver.find_element(:link_text, @sign_in_link).click
+  end
+
+  def input_email_field(text)
+    @chrome_driver.find_element(:name, @create_user_email).send_keys(text)
+  end
+
+  def create_account_field
+    @chrome_driver.find_element(:name, @create_account).click
+  end
+
+  def select_title_field
+    @chrome_driver.find_element(:id, 'id_gender2').click
+  end
+
+end
+
+test = YourLogoFormSelenium.new
+test.visit_your_logo_form
+test.sign_into_your_logo_website
+test.input_email_field('hadedayo@spartaglobal.com')
+test.create_account_field
+test.select_title_field
+sleep 5
